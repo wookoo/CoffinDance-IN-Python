639855e34b94bbbb384d4f27f0ae1ed4e8beafa0
김도현 <39308953+wookoo@users.noreply.github.com>
2020-05-26 15:37:01 +0900

    Pitch UP!

--- CoffinDance.py
+++ CoffinDance.py
@@ -1,55 +1,62 @@
 from winsound import Beep
 import time
 def do (time=200):
-    Beep(261,time)
+    Beep(261*2,time)
+
 def do_sharp (time=200):
-    Beep(277,time)
+    Beep(277*2,time)
+
 def rae (time=200):
-    Beep(293,time)
+    Beep(293*2,time)
+
 def rae_sharp (time=200):
-    Beep(311,time)
+    Beep(311*2,time)
+
 def me (time=200):
-    Beep(329,300)
+    Beep(329*2,time)
+
 def fa (time=time):
-    Beep(349,300)
+    Beep(349*2,time)
+
 def fa_sharp (time=200):
-    Beep(369,time)
+    Beep(369*2,time)
+
 def sol (time=200):
-    Beep(392,time)
+    Beep(392*2,time)
 def sol_sharp (time=200):
-    Beep(415,time)
+    Beep(415*2,time)
 def ra (time=200):
-    Beep(440,time)
+    Beep(440*2,time)
 def ra_shar (time=200):
-    Beep(446,time)
+    Beep(446*2,time)
 def si (time=200):
-    Beep(493,time)
+    Beep(493*2,time)
 
 def high_do (time=200):
-    Beep(523,time)
+    Beep(523*2,time)
 def high_do_sharp (time=200):
-    Beep(554,time)
+    Beep(554*2,time)
 
 def high_rae (time=200):
-    Beep(587,time)
+    Beep(587*2,time)
 def high_rae_sharp (time=200):
-    Beep(622,time)
+    Beep(622*2,time)
 def high_me (time=200):
-    Beep(659,time)
+    Beep(659*2,time)
 def high_fa (time=200):
-    Beep(698,time)
+    Beep(698*2,time)
 def high_fa_sharp (time=200):
-    Beep(739,time)
+    Beep(739*2,time)
 def high_sol (time=200):
-    Beep(783,time)
+    Beep(783*2,time)
 def high_sol_sharp (time=200):
-    Beep(830,time)
+    Beep(830*2,time)
 def high_ra (time=200):
-    Beep(880,time)
+    Beep(880*2,time)
 def high_ra_sharp (time=200):
-    Beep(932,time)
+    Beep(932*2,time)
 def high_si (time=200):
-    Beep(987,time)
+    Beep(987*2,time)
 
 
 def main_melody():
@@ -106,8 +113,9 @@ def sub_melody():
     ra(200)
     sol_sharp(200)
     me(200)
-
-
+print("Made in South Korea")
+print("~~ WOOKOO ~~")
+input()
 main_melody()
 main_melody()
 main_melody()
